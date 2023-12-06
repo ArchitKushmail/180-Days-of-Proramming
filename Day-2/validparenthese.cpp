90e3f61c99dc03f79493051c7082bc370a9e1459
ArchitKushmail <architkushmail1@gmail.com>
2023-12-06 11:49:51 +0530

    Create validparenthese.cpp

---
+++ Day-2/validparenthese.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<stack>
+#include<string>
+using namespace std ;
+bool isValid(string &s) {
+        stack<char>st;
+        int size = s.size();
+        for(int i = 0;i<size;i++){
+            char ch = s[i];
+            // Opening Brackets
+            if(ch == '(' || ch=='{' || ch=='['){
+                st.push(ch);
+            }else {
+                // Closing Bracket
+                if(!st.empty()){
+                    char topElement = st.top();
+                    if(ch==')' && topElement == '('){
+                        st.pop(); 
+                    }else if(ch==']' && topElement == '['){
+                        st.pop();
+                    }else if(ch=='}' && topElement == '{'){
+                        st.pop();
+                    }else{
+                        // No Case Matched
+                        return false;
+                    }
+                }else{
+                    return false;
+                }
+            }
+        }
+        if(st.empty()){
+            // Valid Expression 
+            return true;
+        }
+        return false;
+    }
+int main()
+{
+string s = "()[]{}";
+bool ans = isValid(s);
+cout<<ans<<endl;
+return 0;
+}
