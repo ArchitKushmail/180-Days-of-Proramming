d7e8ffc5acb4d43612cf167bc3d7396a496492f9
ArchitKushmail <architkushmail1@gmail.com>
2023-12-10 10:30:29 +0530

    Create longestvalidparentheses.cpp

---
+++ Day-3/longestvalidparentheses.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std ;
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int>st;
+        st.push(-1);
+        int maxlength = 0;
+        for(int i=0;i<s.size();i++){
+            char ch = s[i];
+            if(ch=='('){
+                st.push(i);    
+            }else{
+                st.pop();
+                if(!st.empty()){
+                    int len = i - st.top();
+                    maxlength = max(len , maxlength);
+                }else{
+                    st.push(i);
+                }
+            }
+        }
+        return maxlength;
+    }
+};
+int main()
+{
+
+return 0;
+}
